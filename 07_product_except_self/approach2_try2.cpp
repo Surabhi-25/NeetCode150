1560465e1acd9c37b5e84d42bd034f391c2ee5a3
Surabhi25 <112242601+Surabhi-25@users.noreply.github.com>
2025-08-25 00:18:57 +0530

    Create approach2_try2.cpp
    final code post optimization

---
+++ 07_product_except_self/approach2_try2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        ans[0]=1;
+        for(int i=1;i<n;++i){
+            ans[i]=nums[i-1]*ans[i-1];
+        }
+
+        int right=1;
+        for(int i=n-2;i>=0;--i){
+            
+            right*=nums[i+1];
+            ans[i]*=right;
+            
+        }
+        return ans;
+
+
+    }
+};
